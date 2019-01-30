e4625fa34e710cee276373a4b68eba0715a88cb7
Stephane Michel <st.fan.michel@gmail.com>
2019-01-30 10:28:15 +0100

    TP1-PROJET Hello world en page web avec module http

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello ESIR !')
+/*
+    Page web : Hello World
+    Inspîré de https://howtonode.org/hello-node
+*/
+
+const http = require('http')
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.end("<html><body>Hello <b>ESIR</b></body></html>\n")
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000)
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
