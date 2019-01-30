0034fb27a715edcab2068e3ed920adc17703f22c
Stephane Michel <st.fan.michel@gmail.com>
2019-01-30 14:37:28 +0100

    TP1-PROJET Hello world avec comptabilisation des requêtes

--- index.js
+++ index.js
@@ -5,10 +5,13 @@
 
 const http = require('http')
 
+let nbRequest = 0
+
 // Configure our HTTP server to respond with Hello World to all requests.
 const server = http.createServer((req, res) => {
+    nbRequest++;
     res.writeHead(200, {"Content-Type": "application/json"})
-    res.end(JSON.stringify({message: 'Hello World !'}))
+    res.end(JSON.stringify({message: 'Hello World !', nbRequest}))
 })
 
 // Listen on port 8000, IP defaults to 127.0.0.1
